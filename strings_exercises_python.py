6c9c0bfd5346c300ca94069f082ac710063153ec
HighnessAtharva <68660002+HighnessAtharva@users.noreply.github.com>
2020-12-29 18:19:37 +0530

    Update strings_exercises_python.py
    Added Ex. 51

--- strings_exercises_python.py
+++ strings_exercises_python.py
@@ -788,3 +788,27 @@ of the delimiter.
 
 my_string = 'this is some text with space as a delimeter'
 print(my_string.rsplit(maxsplit=1))
+
+"""
+Write a Python program to find the first non-repeating character in given string.
+"""
+
+
+def first_non_repeating_character(str1):
+  char_order = []
+  ctr = {}
+  for c in str1:
+    if c in ctr:
+      ctr[c] += 1
+    else:
+      ctr[c] = 1
+      char_order.append(c)
+  for c in char_order:
+    if ctr[c] == 1:
+      return c
+  return None
+
+
+print(first_non_repeating_character('abcdef'))
+print(first_non_repeating_character('abcabcdef'))
+print(first_non_repeating_character('aabbcc'))
