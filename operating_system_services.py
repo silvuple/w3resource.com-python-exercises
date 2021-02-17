826d21e8a4c1f54e4e85344eb280eaea42edef6a
HighnessAtharva <68660002+HighnessAtharva@users.noreply.github.com>
2021-02-17 10:59:34 +0530

    Added OS Services Excercises

---
+++ operating_system_services.py
@@ -0,0 +1,53 @@
+# 1. Write a Python program to get the name of the operating system(Platform independent), information of current operating system, current working directory, print files and directories in the current directory and raises error in the case of invalid or inaccessible file names and paths. 
+
+# 2. Write a Python program to list only directories, files and all directories, files in a specified path. 
+
+# 3. Write a Python program to scan a specified directory and identify the sub directories and files.
+
+# 4. Write a Python program to check for access to a specified path. Test the existence, readability, writability and executability of the specified path. 
+
+# 5. Write a Python program to get the size, permissions, owner, device, created, last modified and last accessed date time of a specified path. 
+
+# 6. Write a Python program to create a symbolic link and read it to decide the original file pointed by the link. 
+
+# 7. Write a Python program to create a file and write some text and rename the file name. 
+
+# 8. Write a Python program to find the parent's process id, real user ID of the current process and change real user ID. 
+
+# 9. Write a Python program to retrieve the current working directory and change the dir(moving up one). 
+
+# 10. Write a python program to access environment variables and value of the environment variable. 
+
+# 11. Write a Python program to iterate over a root level path and print all its sub-directories and files, also loop over specified dirs and files. 
+
+# 12. Write a Python program to test whether a given path exists or not. If the path exist find the filename and directory portion of the said path. 
+
+# 13. Write a Python program to join one or more path components together and split a given path in directory and file. 
+
+# 14. Write a Python program to alter the owner and the group id of a specified file. 
+
+# 15. Write a Python program to get information about the file pertaining to the file mode. Print the information - ID of device containing file, inode number, protection, number of hard links, user ID of owner, group ID of owner, total size (in bytes), time of last access, time of last modification and time of last status change. 
+
+# 16. Write a Python program to write a string to a buffer and retrieve the value written, at the end discard buffer memory. 
+
+# 17. Write a Python program to run an operating system command using the os module. 
+
+# 18. Write a Python program to start a new process replacing the current process. 
+
+# 19. The epoch is the point where the time starts, and is platform dependent. For Unix, the epoch is January 1, 1970, 00: 00: 00 (UTC). Write a Python program to find out what the epoch is on a given platform. Also convert a given time in seconds since the epoch. 
+
+# 20. Write a Python program to get time values with components using local time and gmtime. 
+
+# 21. Write a Python program to get different time values with components timezone, timezone abbreviations, the offset of the local(non-DST) timezone, DST timezone and time of different timezones. 
+
+# 22. Write a Python program that can suspend execution of a given script a given number of seconds. 
+
+# 23. Write a Python program to convert a given time in seconds since the epoch to a string representing local time. 
+
+# 24. Write a Python program to print simple format of time, full names and the representation format and preferred date time format. 
+
+# 25. Write a Python program that takes a given number of seconds and pass since epoch as an argument. Print structure time in local time. 
+
+# 26. Write a Python program that takes a tuple containing 9 elements corresponding to structure time as an argument and returns a string representing it. 
+
+# 27. Write a Python program to parse a string representing time and returns the structure time. 
