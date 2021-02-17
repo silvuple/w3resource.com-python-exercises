02a839fae8fee480146924db7c5e128984c5e18a
HighnessAtharva <68660002+HighnessAtharva@users.noreply.github.com>
2021-02-17 10:56:40 +0530

    Added Request Exercises

---
+++ requests.py
@@ -0,0 +1,28 @@
+# Requests is an elegant and simple HTTP library for Python, built for human beings. Requests allows you to send HTTP/1.1 requests extremely easily. There's no need to manually add query strings to your URLs, or to form-encode your POST data. Keep-alive and HTTP connection pooling are 100 % automatic.
+
+# 1. Write a Python code to find the Requests module - version, licence, copyright information, author, author email, document url, title and description. 
+
+
+# 2. Write a Python code to check the status code issued by a server in response to a client's request made to the server. Print all of the methods and attributes available to objects on successful request. 
+
+
+# 3. Write a Python code to send a request to a web page, and print the response text and content. Also get the raw socket response from the server. 
+
+
+# 4. Write a Python code to send a request to a web page, and print the information of headers. Also parse these values and print key-value pairs holding various information. 
+
+
+# 5. Write a Python code to send a request to a web page, and print the JSON value of the response. Also print each key value of the response. 
+
+
+# 6. Write a Python code to send a request to a web page and stop waiting for a response after a given number of seconds. In the event of times out of request, raise Timeout exception. 
+
+
+# 7. Write a Python code to send some sort of data in the URL's query string. 
+
+
+# 8. Write a Python code to send cookies to a given server and access cookies from the response of a server. 
+
+
+# 9. Write a Python code to verify the SSL certificate for a website which is certified. 
+
